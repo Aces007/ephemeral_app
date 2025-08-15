b878a6c1af4ade8b6ac9db74a294bea6f36dd6f4
Ace Lawrence Zuñiga Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-08-15 22:52:38 +0800

    survey screen added

---
+++ app/screens/SurveyScreens/SurveyOne.tsx
@@ -0,0 +1,64 @@
+import { Roboto_400Regular, Roboto_500Medium, useFonts } from '@expo-google-fonts/roboto';
+import { AntDesign } from '@expo/vector-icons';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+
+const SurveyOne = () => {
+    let [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium,
+    })
+
+
+
+    return (
+        <View style={styles.surveyOneCont}>
+            <View style={styles.surveyOneContent}>
+                <Image source={require('../../../assets/images/misc/medGraphic.png')} style={styles.medGraphic}/>
+                <View style={styles.surveyOneContentBottom}>
+                    <Text style={styles.surveyTagline}>Calm and Peace within Moments</Text>
+                    <TouchableOpacity style={styles.forwardBtn}>
+                        <AntDesign name='arrowright' size={35} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    surveyOneCont: {
+        flexGrow: 1,
+        padding: 20,
+        backgroundColor: "#1A222B"
+    },
+    surveyOneContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 30,
+    },
+    surveyOneContentBottom: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 50,
+    },
+    medGraphic: {
+        width: 350,
+        height: 350,
+    },
+    surveyTagline: {
+        textAlign: 'center',
+        fontFamily: "Roboto_500Medium",
+        fontSize: 30,
+        color: "white",
+    },
+    forwardBtn: {
+        backgroundColor: "#6BD6CF",
+        padding: 10,
+        borderRadius: 50,
+    },
+})
+
+export default SurveyOne;
