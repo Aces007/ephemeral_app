b8b7c2aa5555e56c862da9b3c39fcf638214a1b7
Ace Lawrence Zuñiga Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-08-26 18:32:36 +0800

    adding activities in Space

--- app/(tabs)/Space.tsx
+++ app/(tabs)/Space.tsx
@@ -1,9 +1,90 @@
-const Space = () => {
-    return(
-        <>
+import Header from "@/app/reusables/Header/Header";
+import { StyleSheet, View } from "react-native";
 
-        </>
+type Routine = {
+    title: string;
+    desc: string;
+}
+
+type RoutineSelection = {
+    meditate: Routine[];
+    introspect: Routine[];
+    unwind: Routine[];
+}
+
+type RoutineList = {
+    [date: string]: RoutineSelection
+}
+
+const routines : RoutineSelection[] = [
+    {
+        meditate: [
+            {
+                title: "5 MIN MEDITATION",
+                desc: "A 5-minute pause to breathe and refocus."
+            },
+            {
+                title: "10 MIN MEDITATION",
+                desc: "Take 10 to return to yourself."
+            },
+            {
+                title: "5 MIN BOX BREATHING",
+                desc: "Breathe in calm, one box at a time."
+            },
+            {
+                title: "WALKING MEDITATION",
+                desc: "Move slowly with mindful steps."
+            },
+            {
+                title: "GRATITUDE PRACTICE",
+                desc: "Pause and feel thankful."
+            },
+            {
+                title: "MINDFUL BREATHING",
+                desc: "Focus gently on your breath."
+            },
+        ],
+        introspect: [
+            {
+                title: "GRATITUDE PRACTICE",
+                desc: "A moment to appreciate the good in your life."
+            }
+        ],
+        unwind: [
+            {
+                title: "GRATITUDE PRACTICE",
+                desc: "A moment to appreciate the good in your life."
+            }
+        ]
+    },
+]
+
+
+const Space = () => {
+    
+
+    return(
+        <View style={styles.spaceCont}>
+            <Header variant="Space" title="Space" subtitle="Mind Sanctuary" />
+
+            <View style={styles.spaceContent}>
+
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    spaceCont: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#101B29",
+    },
+    spaceContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 32,
+    },
+})
 
 export default Space;
--- app/(tabs)/You.tsx
+++ app/(tabs)/You.tsx
@@ -1,9 +1,29 @@
+import { StyleSheet, View } from "react-native";
+import Header from "../reusables/Header/Header";
+
 const You = () => {
     return(
-        <>
+        <View style={styles.youCont}>
+            <Header variant="You" title="You" subtitle="Inner Growth" />
 
-        </>
+            <View style={styles.youContent}>
+
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    youCont: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#101B29",
+    },
+    youContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 32,
+    }
+})
 
 export default You;
