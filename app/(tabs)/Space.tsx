d3c105839468d3c80e3ab4f7a7d549c3421dc3eb
Ace Lawrence Zuñiga Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-08-26 18:54:14 +0800

    space menu created

--- app/(tabs)/Space.tsx
+++ app/(tabs)/Space.tsx
@@ -1,5 +1,8 @@
 import Header from "@/app/reusables/Header/Header";
-import { StyleSheet, View } from "react-native";
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+import { Entypo, Fontisto, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 type Routine = {
     title: string;
@@ -46,9 +49,21 @@ const routines : RoutineSelection[] = [
         ],
         introspect: [
             {
-                title: "GRATITUDE PRACTICE",
-                desc: "A moment to appreciate the good in your life."
-            }
+                title: "SET GOALS",
+                desc: "Set gentle direction, not pressure."
+            },
+            {
+                title: "DAILY GRATITUDE",
+                desc: "Notice the good, even briefly."
+            },
+            {
+                title: "REFLECT",
+                desc: "Unfold your thoughts without judgment."
+            },
+            {
+                title: "INSIGHTS",
+                desc: "Store what inspires quietly."
+            },
         ],
         unwind: [
             {
@@ -61,13 +76,36 @@ const routines : RoutineSelection[] = [
 
 
 const Space = () => {
+    const [selected, setSelected] = useState(false);
     
+    let [fontsLoaded] = useFonts({
+        Inter_500Medium
+    }); if (!fontsLoaded) {
+        return;
+    }
 
     return(
         <View style={styles.spaceCont}>
             <Header variant="Space" title="Space" subtitle="Mind Sanctuary" />
 
             <View style={styles.spaceContent}>
+                <View style={styles.spaceMenu}>
+                    <TouchableOpacity style={styles.spaceMenuBtns}>
+                        <Entypo name="air" size={24} style={styles.spaceMenuSVG}/>
+                        <Text style={styles.spaceMenuLabel}>Meditate</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.spaceMenuBtns}>
+                        <MaterialCommunityIcons name="notebook-edit-outline" size={24} style={styles.spaceMenuSVG}/>
+                        <Text style={styles.spaceMenuLabel}>Introspect</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.spaceMenuBtns}>
+                        <Fontisto name="wind" size={24} style={styles.spaceMenuSVG}/>
+                        <Text style={styles.spaceMenuLabel}>Unwind</Text>
+                    </TouchableOpacity>
+                </View>
+
 
             </View>
         </View>
@@ -85,6 +123,28 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         gap: 32,
     },
+    spaceMenu: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        gap: 8,
+    },
+    spaceMenuBtns: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#A994E9',
+        padding: 16,
+        borderRadius: 8,
+    },
+    spaceMenuSVG: {
+        color: '#F0F0F0',
+    },
+    spaceMenuLabel: {
+        color: '#F0F0F0',
+        fontFamily: 'Inter_500Medium',   
+    }
 })
 
 export default Space;
