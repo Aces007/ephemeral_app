ce58ee9ef7a76e303ca73cb2e2a2ea95ee91bb1d
Ace Lawrence Zuñiga Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-10-03 21:57:14 +0800

    added eas, few tweaks

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,21 +1,25 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getReactNativePersistence, initializeAuth } from "firebase/auth/react-native";
+import { getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth/react-native";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAXfzheReHset_z2ew5A1kMpRK-Y-5HlYU",
     authDomain: "ephemeral-atr41n.firebaseapp.com",
     projectId: "ephemeral-atr41n",            
-    // storageBucket: "ephemeral-12345.appspot.com", 
     messagingSenderId: "607930601313",        
     appId: "1:607930601313:android:8cf0f659239ec67df17f20"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
+let auth;
+try {
+ auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)    
 });
+} catch (error: any) {
+    auth = getAuth(app);
+}
 
-export { auth };
-export default app;
+export { app, auth };
+
